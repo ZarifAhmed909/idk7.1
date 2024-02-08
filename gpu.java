930400df07db7f32dfee2f69271e49497f827afd
Zarif Ahmed <144474613+ZarifAhmed909@users.noreply.github.com>
2024-02-08 12:33:02 -0500

    Create gpu.java

---
+++ gpu.java
@@ -0,0 +1,9 @@
+public class gpu {
+    private String brand;
+    private int series;
+
+    public gpu(String brand, int series){
+        this.brand = brand;
+        this.series = series;
+    }
+}
